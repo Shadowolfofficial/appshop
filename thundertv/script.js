77fd9c3fa606cac3be34cbb6aba7490a19a514e1
ThunderGlitch <107752514+Shadowolfofficial@users.noreply.github.com>
2024-07-02 16:02:44 +0200

    Add files via upload

---
+++ thundertv/script.js
@@ -0,0 +1,25 @@
+        function search_apps() {
+          let input = document.getElementById('searchbar').value.toLowerCase();
+          let x = document.getElementsByClassName('app-card');
+          
+          for (let i = 0; i < x.length; i++) { 
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+              x[i].style.display = "none";
+            } else {
+              x[i].style.display = "block";                 
+            }
+          }
+        }
+
+        function search_installapps() {
+          let input = document.getElementById('searchbar').value.toLowerCase();
+          let x = document.getElementsByClassName('install-apps');
+          
+          for (let i = 0; i < x.length; i++) { 
+            if (!x[i].innerHTML.toLowerCase().includes(input)) {
+              x[i].style.display = "none";
+            } else {
+              x[i].style.display = "block";                 
+            }
+          }
+        }
